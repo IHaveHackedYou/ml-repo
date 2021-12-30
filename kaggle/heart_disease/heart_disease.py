369f04926a2f231385048a2127cf376aa847d7ee
flopp <hackeranonym@protonmail.com>
2021-12-30 16:48:15 +0100

    comit

--- kaggle/heart_disease/heart_disease.py
+++ kaggle/heart_disease/heart_disease.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
 heart = pd.read_csv("heart.csv")
 # display in pd functions all columns
 pd.set_option("display.max_columns", None)
+# heart.hist(bins=100, figsize=(20, 15))
 
-heart.hist(bins=100, figsize=(20,15))
-plt.show()
+# shuffle dataset
+heart = heart.sample(frac=1).reset_index(drop=True)
+
+corr_matrix = heart.corr()
+# printcorr_matrix["HeartDisease"].sort_values(ascending=False)
