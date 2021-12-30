bf5352c654f6d0f1f935c6dd389d34601a211a01
flopp <hackeranonym@protonmail.com>
2021-12-30 18:59:34 +0100

    comit

--- kaggle/heart_disease/heart_disease.py
+++ kaggle/heart_disease/heart_disease.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OrdinalEncoder, StandardScaler
+from sklearn.linear_model import LinearRegression
 
 heart = pd.read_csv("heart.csv")
 # display in pd functions all columns
@@ -16,22 +19,44 @@ corr_matrix = heart.corr()
 # print(heart.describe())
 # print(heart.head())
 
+heart_prepared = heart.drop("HeartDisease", axis=1)
+heart_labels = heart["HeartDisease"]
+columns_prepared = heart_prepared.columns
 # ordinal encoder to make e.g. in column "sex" from "m" and "w" 1 and 0
 ordinal_encoder = OrdinalEncoder()
-heart_encoded = ordinal_encoder.fit_transform(heart)
-heart_encoded = pd.DataFrame(heart_encoded, columns=heart.columns)
+heart_encoded = ordinal_encoder.fit_transform(heart_prepared)
+heart_encoded = pd.DataFrame(heart_encoded, columns=columns_prepared)
 # heart_index = heart.reset_index()
 # print(heart_encoded.head())
 
 # unify deviation
 standard_scaler = StandardScaler()
 heart_prepared = standard_scaler.fit_transform(heart_encoded)
-heart_prepared = pd.DataFrame(heart_prepared, columns=heart.columns)
-print(heart_prepared.head())
+heart_prepared = pd.DataFrame(heart_prepared, columns=columns_prepared)
+# print(heart_prepared.head())
 heart_prepared.hist(bins=100, figsize=(20, 15))
-# plt.show()
 
-housing_labels = heart.drop("HeartDisease", axis=1)
+# shuffle dataset
+heart_prepared = heart_prepared.sample(frac=1).reset_index(drop=True)
 
-X_train, X_test, y_train, y_test = train_test_split(heart_prepared, housing_labels, test_size=0.2)
+# split data in train and test set
+X_train, X_test, y_train, y_test = train_test_split(heart_prepared, heart_labels, test_size=0.2)
+
+lin_reg = LinearRegression()
+lin_reg.fit(X_train, y_train)
+
+
+lin_predictions = lin_reg.predict(X_test)
+lin_mse = mean_squared_error(y_test, lin_predictions)
+lin_rmse = np.sqrt(lin_mse)
+print(lin_rmse)
+
+def prepare_predict(data, model):
+    ordinal_encoder = OrdinalEncoder()
+    data_encoded = ordinal_encoder.fit_transform(data)
+    standard_scaler = StandardScaler()
+    data_prepared = standard_scaler.fit_transform(data_encoded)
+    return model.predict(data_prepared)
+
+
 
