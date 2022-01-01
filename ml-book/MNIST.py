401668cb4b0c1693957ad382ce8ca8513063c43f
flopp <hackeranonym@protonmail.com>
2022-01-01 13:14:08 +0100

    comit

--- ml-book/MNIST.py
+++ ml-book/MNIST.py
@@ -10,10 +10,9 @@ mnist = fetch_openml("mnist_784", version=1)
 
 X, y = mnist["data"], mnist["target"]
 
-print(X.shape)
-'''some_digit = X[0]
-some_digit_image = some_digit.reshape(28, 28)
-plt.imshow(some_digit_image, cmap="binary")
-plt.axis("off")
-plt.show()
-'''
+print(type(X))
+# some_digit = X[0]
+# some_digit_image = some_digit.reshape(28, 28)
+# plt.imshow(some_digit_image, cmap="binary")
+# plt.axis("off")
+# plt.show()
